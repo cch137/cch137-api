910390fc6a85b1f88f5ac447ba01db198f5aba3d
cch137 <96408836+cch137@users.noreply.github.com>
2024-02-14 17:53:36 +0800

    fix: cleanup

--- src/utils/parseForm.ts
+++ src/utils/parseForm.ts
@@ -3,27 +3,28 @@ import type { ParsedQs } from "qs";
 
 type ParsedQsValue = string | ParsedQs | string[] | ParsedQs[] | undefined;
 
-const tryParseJSON = (obj: any) => {
-  try {
-    if (typeof obj === "string") return JSON.parse(obj);
-  } catch {}
-  return obj;
-};
-
 const tryParseQsToJSON = (q: ParsedQsValue): any => {
   if (!q) return q;
-  if (typeof q === "string") return tryParseJSON(q);
+  if (typeof q === "string") {
+    try {
+      return JSON.parse(q);
+    } catch {
+      return q;
+    }
+  }
   if (Array.isArray(q)) return q.map((i) => tryParseQsToJSON(i));
   for (const k in q) q[k] = tryParseQsToJSON(q[k]);
   return q;
 };
 
 const parseForm = <T extends { [key: string]: any }>(req: Request) => {
-  const _body: Partial<T> = {};
-  const { query, body } = req;
-  for (const key in query) _body[key as keyof T] = tryParseQsToJSON(query[key]);
-  for (const key in body) _body[key as keyof T] = tryParseJSON(body[key]);
-  return _body;
+  const { query, body: _body } = req;
+  const body: Partial<T> = typeof _body === "object" ? { ..._body } : {};
+  for (const key in query) {
+    if (key in body) continue;
+    body[key as keyof T] = tryParseQsToJSON(query[key]);
+  }
+  return body;
 };
 
 export default parseForm;
