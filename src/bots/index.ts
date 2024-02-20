5b1128cd3fa935aa671fb68000ab83c623973e05
cch137 <96408836+cch137@users.noreply.github.com>
2024-02-20 11:07:44 +0800

    feat: new member role

--- src/bots/index.ts
+++ src/bots/index.ts
@@ -1,9 +1,11 @@
-import ch4, {run as ch4Run} from "./ch4";
-import curva, {run as curva4Run} from "./curva";
+import ch4, { run as ch4Run } from "./ch4";
+import curva, { run as curvaRun } from "./curva";
 
-export {
-  ch4,
-  curva,
+export { ch4, curva };
+
+export const run = () => {
+  const bots: Promise<void>[] = [];
+  bots.push(ch4Run());
+  bots.push(curvaRun());
+  return Promise.all(bots);
 };
-
-export const run = () => [ch4Run(), curva4Run()];
