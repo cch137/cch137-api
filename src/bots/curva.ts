b3dbd74c370b3d4f80cc1de997ddd1d55b4169ab
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-11 17:15:33 +0800

    fix: dont reply when @everyone

--- src/bots/curva.ts
+++ src/bots/curva.ts
@@ -171,6 +171,7 @@ curva.on(Events.ClientReady, async () => {
       listeningChannelIds.has(channel.id) &&
       !answeringChannelIds.has(channel.id);
     if (!isTriggeredByListening && !message.mentions.has(user.id)) return;
+    if (/@everyone/.test(message.content)) return;
     answeringChannelIds.add(channel.id);
     const typing = startTyping(channel);
     let replied = channel.send(createInfoEmbed("Thinking..."));
